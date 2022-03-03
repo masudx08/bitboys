ccac9450cee9aadc37502c2600bddda01753b3c9
masudx08 <promasud225@gmail.com>
2022-03-03 22:51:05 +0600

    about done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,38 +4,44 @@ import './about.css'
 export default function About() {
   return (
     <div className='aboutC'>
-      <div>
-        <h1 >Kynno Enterprise Grade <br /> Blockchain</h1>
+      <div className="inner">
+        <div>
+          <h1 >Kynno Enterprise Grade <br /> Blockchain</h1>
+        </div>
+        <Row >
+          <Col>
+            <div>
+              <p style={{fontWeight:'bold'}}>
+                Kynno is a revolutionary decentralized permissionless enterprise
+                grade blockchain platform, built to support and operate secure, 
+                compliant, and cutting-edge digital infrastructures.
+              </p>
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <p>
+                Kynno is a revolutionary decentralized permissionless enterprise
+                grade blockchain platform, built to support and operate secure, 
+                compliant, and cutting-edge digital infrastructures.
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <div>
+            <div className='setbg aboutBottom'>
+              <div className='inner'>
+                <h4>What is the objective of the KYNNO blockchain?</h4>
+                <p>
+                  Kynno’s main objective is to interface and communicate with other 
+                  blockchains and wallets to gather data on all your assets. This data
+                  is then sent to one centralized dashboard on your Kynno Data Hub
+                  to be analyzed, organized and displayed.
+                </p>
+              </div>
+            </div>
+        </div>
       </div>
-      <Row className='setbg'>
-        <Col>
-          <div>
-            <p>
-              Kynno is a revolutionary decentralized permissionless enterprise
-              grade blockchain platform, built to support and operate secure, 
-              compliant, and cutting-edge digital infrastructures.
-            </p>
-          </div>
-        </Col>
-        <Col>
-          <div>
-            <p>
-              Kynno is a revolutionary decentralized permissionless enterprise
-              grade blockchain platform, built to support and operate secure, 
-              compliant, and cutting-edge digital infrastructures.
-            </p>
-          </div>
-          <div >
-            <h4>What is the objective of the KYNNO blockchain?</h4>
-            <p>
-              Kynno’s main objective is to interface and communicate with other 
-              blockchains and wallets to gather data on all your assets. This data
-              is then sent to one centralized dashboard on your Kynno Data Hub
-              to be analyzed, organized and displayed.
-            </p>
-          </div>
-        </Col>
-      </Row>
     </div>
   )
 }
