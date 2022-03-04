0d6797e33deeda58edbf18936194c3dc40bd4fd6
masudx08 <promasud225@gmail.com>
2022-03-04 09:23:41 +0600

    technologies done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,53 +3,64 @@ import './technologies.css'
 export default function Technologies() {
   return (
     <div className='techC'>
-     <div className='setbg-1'>
-        <div>
-            <h1>Kynno Zkps Technologies</h1>
-            <p>Storing personal data in a decentralized way while guaranteeing its security 
-              is made possible by combining Kynno’s disruptive blockchain technology and 
-              Zero Knowledge Proof Protocols.
-            </p>
-            <p>
-              ‍Our network processes data, transactions, runs applications, operates 
-              as a repository and serves as the regulating point of entry into 
-              Kynno’s data ecosystem.
-            </p>
+      <div>
+        <div className='setbg-1'>
+          <div className="inner1">
+            <div className="left">
+              <div>
+                <h1>Kynno Zkps Technologies</h1>
+                <p>Storing personal data in a decentralized way while guaranteeing its security
+                  is made possible by combining Kynno’s disruptive blockchain technology and
+                  Zero Knowledge Proof Protocols.
+                </p>
+                <p>
+                  ‍Our network processes data, transactions, runs applications, operates
+                  as a repository and serves as the regulating point of entry into
+                  Kynno’s data ecosystem.
+                </p>
+              </div>
+
+              <div className='techBox'>
+                <h4>Grant and Delegation Programs:</h4>
+                <p>
+                  The Kynno Foundation will reserve funds for R&D
+                  initiatives led by third parties that bring value to the
+                  ecosystem’s growth.
+                </p>
+              </div>
+              <div className='techBox'>
+                <h4>Research:</h4>
+                <p>
+                  The Kynno Foundation will reserve funds for R&D
+                  initiatives led by third parties that bring value to the
+                  ecosystem’s growth.
+                </p>
+              </div>
+            </div>
+            <div className="right">
+              <div className='techBox'>
+                <h4>Education and Advancement:</h4>
+                <p>
+                  A Kynno school to educate people, companies and
+                  governments and leverage knowledge to push for
+                  decentralized technologies’ mass adoption.
+                </p>
+              </div>
+            </div>
           </div>
-          <div>
-            <h4>Grant and Delegation Programs:</h4>
-            <p>
-              The Kynno Foundation will reserve funds for R&D 
-              initiatives led by third parties that bring value to the 
-              ecosystem’s growth.
-            </p>
+        </div>
+        <div className='setbg-2'>
+          <div className="inner2">
+            <h2>
+              One unique platform connected
+              to all ecosystems and centralizing
+              your web 3.0 identity, assets, data
+              and legal documents efficiently
+              and securely.
+            </h2>
+            <i>Leadership Team Of KYNNO</i>
           </div>
-          <div>
-            <h4>Research:</h4>
-            <p>
-              The Kynno Foundation will reserve funds for R&D 
-              initiatives led by third parties that bring value to the 
-              ecosystem’s growth.
-            </p>
-          </div>
-          <div>
-            <h4>Education and Advancement:</h4>
-            <p>
-              A Kynno school to educate people, companies and 
-              governments and leverage knowledge to push for 
-              decentralized technologies’ mass adoption.
-            </p>
-          </div>
-     </div>
-      <div className='setbg-2'>
-        <h2>
-          One unique platform connected
-          to all ecosystems and centralizing
-          your web 3.0 identity, assets, data
-          and legal documents efficiently
-          and securely.
-        </h2>
-        <i>Leadership Team Of KYNNO</i>
+        </div>
       </div>
     </div>
   )
