f42044215d4daa0fa364466ca46b9e3a851b9911
masudx08 <promasud225@gmail.com>
2022-03-04 12:24:43 +0600

    all done without perfect ui

--- src/components/_common/Slider/CustomSlider.js
+++ src/components/_common/Slider/CustomSlider.js
@@ -2,31 +2,44 @@ import React from 'react'
 import './customSlider.css'
 import Line from '../../../images/line.png'
 export default function CustomSlider() {
+  const arr = [1,2,3,4,5,6,7,8,9,10]
   return (
-    <div className='sliderC'>
-      <div>
-        <img src={Line} alt="" />
-      </div>
-      <h4>All-in-one Crypto Tracker</h4>
-      <p>
-        All your assets and transactions accessible at a glance
-        spread across wallets, blockchains and marketplaces.
-      </p>
-      <b>
-        Live Cryptofolio and Transaction History: 
-      </b>
-      <p>
-        Real Time visibility of all your assets in one dashboard. 
-        Search, view and filter all your past transactions. 
-      </p>
-      <h4>
-        Alerts: 
-      </h4>
-      <p>
-        Get Alerts when new interesting assets are available, 
-        when an asset is in a buy or sell zone or a market trend
-        is shifting.
-      </p>
+    <div className='slider-container'>
+      
+        {
+          arr.map(item=>{
+            return (
+              <div className='sliderC'>
+              <div key={item}>
+                <div>
+                  <img src={Line} alt="" />
+                </div>
+                <h4>All-in-one Crypto Tracker</h4>
+                <p>
+                  All your assets and transactions accessible at a glance
+                  spread across wallets, blockchains and marketplaces.
+                </p>
+                <b>
+                  Live Cryptofolio and Transaction History: 
+                </b>
+                <p>
+                  Real Time visibility of all your assets in one dashboard. 
+                  Search, view and filter all your past transactions. 
+                </p>
+                <h4>
+                  Alerts: 
+                </h4>
+                <p>
+                  Get Alerts when new interesting assets are available, 
+                  when an asset is in a buy or sell zone or a market trend
+                  is shifting.
+                </p>
+                </div>
+                </div>
+            )
+          })
+        }
+      
     </div>
   )
 }
